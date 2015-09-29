b0ab9419bc567f922b9664c5dd13a13916247d36
Douglas Gontijo <douggr@users.noreply.github.com>
2015-09-29 09:33:52 -0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# harp-bp
+### Command line interface for creating new projects from scratch.
+#### Why?
+Often, starting a new project requires the same boilerplate code over and over.
+This is helpful if you want to create a project with a sane starting point and
+common defaults.
+
+## Prerequisites
+  1. [Node.js](https://nodejs.org)
+  2. [gulp](http://gulpjs.com)
+  3. [harp](http://harpjs.com) - optional
+
+## tl;dr
+Open a terminal type the following:
+```sh
+git clone https://github.com/douggr/harp-bp my-app
+cd my-app
+npm install
+gulp
+```
+Point your browser to http://localhost:3000
+
+## Development Setup
+Bring up a terminal and type:
+```sh
+git clone https://github.com/douggr/harp-bp my-app
+# or
+harp init my-app -b douggr/harp-bp
+```
+
+1. cd into `my-app`
+2. install local dependencies by running `npm install`
+3. run `gulp`
+4. open http://localhost:3000
+
+## Preprocessors
+- Markdown
+- EJS
+- **Jade** (default)
+- LESS
+- **Sass** (default)
+- Stylus
+- CoffeeScript
---
+++ gulpfile.js
@@ -0,0 +1,125 @@
+'use strict'
+
+var $           = require('gulp-load-plugins')()
+var browserSync = require('browser-sync')
+var del         = require('del')
+var gulp        = require('gulp')
+var harp        = require('harp')
+var reload      = browserSync.reload
+var wiredep     = require('wiredep').stream
+
+/**
+ * Wire Bower dependencies to `public/_bower-*` files.
+ */
+gulp.task('wiredep', function () {
+  try {
+    // wiredep fails miserably if he can't find `bower_components`
+    var fs = require('fs')
+    var path = fs.statSync('public/bower_components')
+    if (path.isDirectory()) {
+      return gulp
+        .src(['public/_bower-*'])
+        .pipe(wiredep())
+        .pipe(gulp.dest('public/'))
+    }
+  } catch (e) {
+    // bah
+  }
+})
+
+/**
+ * Replace bower refs to resources on a CDN.
+ */
+gulp.task('cdnify', function () {
+  return gulp
+    .src(['public/_bower-*'])
+    .pipe($.googleCdn(require('./bower.json'), {
+      cdn: require('jsdelivr-cdn-data')
+    }))
+    .pipe(gulp.dest('public/'));
+})
+
+/**
+ * Clean output directories
+ */
+gulp.task('clean', del.bind(null, ['dist', '.publish'], {dot: true}))
+
+/**
+ * Export your site to flat static assets (HTML/CSS/JavaScript)
+ */
+gulp.task('harp:compile', ['clean'], function () {
+  harp.compile(__dirname, 'dist', function () {
+    del('dist/bower_components', {
+      force: true
+    })
+  })
+})
+
+/**
+ * Serve the site from the `public` directory
+ */
+gulp.task('serve', ['wiredep'], function () {
+  harp.server(__dirname, {
+    port: 9000
+  }, function () {
+    browserSync({
+      proxy: 'localhost:9000',
+      open: false,
+      // Hide the notification. It gets annoying
+      notify: {
+        styles: ['opacity:0', 'position:absolute']
+      }
+    })
+
+    gulp.watch(['public/assets/css/**/*.scss'], function () {
+      reload('main.css', {stream: true})
+    })
+
+    gulp.watch(['public/**/*.{jade,ejs,md,html}'], function () {
+      reload()
+    })
+
+    gulp.watch(['bower.json'], ['wiredep'])
+  })
+})
+
+/**
+ * Build production files
+ */
+gulp.task('build', ['clean', 'bump'], function (cb) {
+  require('run-sequence')(
+    'wiredep',
+    ['cdnify'],
+    'harp:compile',
+    cb
+  )
+})
+
+/**
+ * Default task, running `gulp` will fire up the Harp site,
+ * launch BrowserSync & watch files.
+ */
+gulp.task('default', ['serve'])
+
+/**
+ * Bump the app version
+ */
+gulp.task('bump', function () {
+  if (!$.util.env.r) {
+    return
+  }
+
+  var semver  = require('semver')
+  var pkg     = require('./package.json')
+  var version = semver.inc(pkg.version, $.util.env.r)
+
+  gulp
+    .src(['./*.json'])
+    .pipe(
+      $.replace(
+        '"version": "' + pkg.version,
+        '"version": "' + version
+      )
+    )
+    .pipe(gulp.dest('./'))
+})
